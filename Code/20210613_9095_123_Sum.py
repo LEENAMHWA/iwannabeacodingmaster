2a8b3b72204afb8b280ea624cb7f0465583c223a
이남화 <namhwa_lee@MacBook-Pro.local>
2021-06-29 16:48:22 +0900

    Code

--- Code/20210613_9095_123_Sum.py
+++ Code/20210613_9095_123_Sum.py
@@ -1,5 +1,18 @@
+# Dynamic Programming
+
 import sys
 
-n = int(sys.stdin.readline().split()[0])
-for test_idx in range(n):
+t = int(sys.stdin.readline().split()[0])
+for test_idx in range(t):
     num = int(sys.stdin.readline().split()[0])
+    answer = 0
+    s = [1,2,4]
+
+    if num <= 3:
+        answer = s[num-1]
+    else:
+        for l_idx in range(3,num):
+            s.append(sum(s[l_idx-3:l_idx]))
+        answer = s[num-1]        
+
+    print(answer)
