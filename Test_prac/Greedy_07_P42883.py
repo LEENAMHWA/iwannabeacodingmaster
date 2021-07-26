e98f5d7c407926fee9a4c31ec027ae54e875cf37
LEENAMHWA <namhwa7176@gmail.com>
2021-07-26 10:46:13 +0900

    Code

---
+++ Test_prac/Greedy_07_P42883.py
@@ -0,0 +1,9 @@
+def solution(number, k):
+    answer = ''
+    stk = []
+    for i in number:
+        while stk and stk[-1] < i and k>0:
+            k-=1
+            stk.pop()
+        stk.append(i)
+    return "".join(stk[:len(stk)-k])
